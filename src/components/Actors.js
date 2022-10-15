2841956448195275e09d8955c1c99d3011fd446f
jordanlynne9@gmail.com <jordanlynne9@gmail.com>
2022-10-15 16:43:26 -0700

    Completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const retreiveActors = actors.map((actor) =>(
+    <div key={actor.name}>
+      <h2>Name: {actor.name}</h2>
+      <h3> Movies: </h3>
+      <ul>
+        {actor.movies.map((movie) =>(
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+
+
+  return( 
+  <div>
+    <h1> Actors Page </h1>
+    {retreiveActors}
+  </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,65 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  color : 'blue'
+}
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className = 'navbar'>
+      <div>
+        <li>
+          <NavLink 
+          to = '/home'
+          exact
+          style = {linkStyles}
+          activeStyle={{ 
+          color: 'purple' 
+          }}
+          > 
+          Home 
+          </NavLink>
+        </li>
+      </div>
+      <div>
+        <li>
+          <NavLink 
+          to = '/actors'
+          exact
+          style = {linkStyles}
+          activeStyle={{ color: 'purple'}}
+          > 
+          Actors 
+          </NavLink>
+        </li>
+      </div>
+      <div>
+        <li>
+          <NavLink 
+          to = '/directors'
+          exact
+          style = {linkStyles}
+          activeStyle={{ color: 'purple' }}
+          > 
+          Directors 
+          </NavLink>
+        </li>
+      </div>
+      <div>
+        <li>
+          <NavLink 
+          to = '/movies'
+          exact
+          style = {linkStyles}
+          activeStyle={{ color: 'purple' }}
+          > 
+          Movies
+          </NavLink>
+        </li>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
